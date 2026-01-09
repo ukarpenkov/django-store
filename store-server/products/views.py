dcbb70ce5be37da762dbcc9ca6a2ebb77b8c32f7
ukarpenkov <truloveu@gmail.com>
2026-01-09 14:14:12 +0300

    refactor templates and views to use Django static template tags for asset loading

--- store-server/products/views.py
+++ store-server/products/views.py
@@ -13,19 +13,19 @@ def products(request):
             {
                 "name": "Худи черного цвета с монограммами adidas Originals",
                 "price": 6090,
-                "image_url": "/static/vendor/img/products/Adidas-hoodie.png",
+                "image_url": "/vendor/img/products/Adidas-hoodie.png",
                 "description": "Черное худи с монограммами adidas Originals, выполненное из мягкого хлопкового флиса. Модель с капюшоном и длинными рукавами оформлена фирменными 3 полосками на рукавах и крупным логотипом Trefoil спереди.",
             },
             {
                 "name": "Синяя куртка The North Face",
                 "price": 23725,
-                "image_url": "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
+                "image_url": "/vendor/img/products/Blue-jacket-The-North-Face.png",
                 "description": "Синяя куртка The North Face из прочного материала с водоотталкивающей пропиткой. Модель с капюшоном, застежкой на молнию и эластичными манжетами защитит вас от ветра и дождя во время прогулок по городу и активного отдыха на природе.",
             },
             {
                 "name": "Коричневый спортивный oversized-топ ASOS DESIGN",
                 "price": 3390,
-                "image_url": "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
+                "image_url": "/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
                 "description": "Коричневый спортивный oversized-топ ASOS DESIGN из мягкого хлопкового джерси. Модель с круглым вырезом и короткими рукавами оформлена контрастной отделкой и логотипом ASOS DESIGN на груди.",
             },
         ],
