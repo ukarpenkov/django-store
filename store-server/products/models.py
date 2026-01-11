9525aed5250cb8069afd92366e2a50c33ab772fc
ukarpenkov <truloveu@gmail.com>
2026-01-11 20:26:11 +0300

    add models ProductCategory and Product

--- store-server/products/models.py
+++ store-server/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="product_images/", null=True, blank=True)
+    category = models.ForeignKey(
+        ProductCategory,
+        related_name="products",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return str(self.name)
