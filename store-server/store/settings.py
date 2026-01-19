580d17e6e693f6273fe75edca5d1bd3434a5bc2b
ukarpenkov <truloveu@gmail.com>
2026-01-19 22:08:19 +0300

    Configure MEDIA_URL and MEDIA_ROOT
    Modify products.html for new imgs showing

--- store-server/store/settings.py
+++ store-server/store/settings.py
@@ -119,4 +119,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- store-server/store/urls.py
+++ store-server/store/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from products.views import index, products
 
+# from store import settings
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', products, name='products'),
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("products/", products, name="products"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
