43019368e929b3fb4ce281c9fb4b3b866949d7a8
ukarpenkov <truloveu@gmail.com>
2026-01-25 12:42:49 +0300

    Update product URLs to use namespaced routing and fix navigation links

--- store-server/products/urls.py
+++ store-server/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "products"
+
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('products', views.products, name='products'),
+    path("", views.products, name="index"),
 ]
