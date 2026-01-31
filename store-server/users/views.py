fe0209871973c64156ae95dfb9bc28a4d3aa6b27
ukarpenkov <truloveu@gmail.com>
2026-01-31 22:37:13 +0300

    Add user login form and update login view to render it

--- store-server/users/views.py
+++ store-server/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, "users/login.html")
+    form = UserLoginForm()
+    return render(request, "users/login.html", {"form": form})
 
 
 def register(request):
