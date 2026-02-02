ef12b5fac10e5098b48705a07470444759b1c79e
ukarpenkov <truloveu@gmail.com>
2026-02-02 18:42:10 +0300

    Refactor login form to use Django form fields and update HTML template for better integration

--- store-server/users/forms.py
+++ store-server/users/forms.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
+from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label="Username",
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={"class": "form-control py-4", "placeholder": "Enter your username"}
+        ),
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control py-4", "placeholder": "Enter your password"}
+        ),
+    )
+
     class Meta:
         model = User
         fields = ["username", "password"]
