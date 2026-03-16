6336bfb911aa12f9425a71ae553b0ac3972cbe3c
ukarpenkov <truloveu@gmail.com>
2026-03-16 17:25:36 +0300

    change db to PostgreSQL and migrate data

--- store-server/store/settings.py
+++ store-server/store/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "store.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "store_db"),
+        "USER": os.environ.get("DB_USER", "store_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "your_password"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
