15e26f2fbd51154908d4e932cce9446b09d8472d
ukarpenkov <truloveu@gmail.com>
2026-04-07 21:23:55 +0300

    Order model

--- store-server/orders/models.py
+++ store-server/orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Order(models.Model):
+    CREATED = 0
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+        (CREATED, "Создан"),
+        (PAID, "Оплачен"),
+        (ON_WAY, "В пути"),
+        (DELIVERED, "Доставлен"),
+    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=100)
+    basket_history = models.JSONField(default=dict)
+    status = models.SmallIntegerField(choices=STATUSES, default=CREATED)
+    created = models.DateTimeField(auto_now_add=True)
+    initiator = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.initiator.username}"
--- store-server/orders/views.py
+++ store-server/orders/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 
+from orders.form import OrderForm
 
 class OrderCreateView(TemplateView):
     template_name = "orders/order-create.html"
@@ -7,6 +8,7 @@ class OrderCreateView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Store - Оформление заказа"
+        context["form"] = OrderForm()
         return context
 
 
