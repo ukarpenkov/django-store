fc0520ae2e8e93301806067d088159fc0fa5725a
ukarpenkov <truloveu@gmail.com>
2026-04-10 18:49:07 +0300

    order create logic

--- store-server/orders/admin.py
+++ store-server/orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email", "status", "created", "initiator")
+    list_filter = ("status", "created")
+    search_fields = ("id", "first_name", "last_name", "email", "initiator__username")
+    readonly_fields = ("created",)
--- store-server/orders/views.py
+++ store-server/orders/views.py
@@ -1,15 +1,68 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
 from orders.form import OrderForm
+from orders.models import Order
+from products.models import Basket
 
-class OrderCreateView(TemplateView):
+
+class OrderCreateView(LoginRequiredMixin, FormView):
     template_name = "orders/order-create.html"
+    form_class = OrderForm
+    success_url = reverse_lazy("orders:order-success")
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == "GET" and not Basket.objects.filter(
+            user=request.user
+        ).exists():
+            return redirect("products:basket")
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        basket_items = list(
+            Basket.objects.filter(user=self.request.user)
+            .select_related("product")
+            .order_by("-created_timestamp")
+        )
+        context["basket"] = basket_items
+        context["basket_total_sum"] = sum((item.sum for item in basket_items), 0)
+        context["basket_total_quantity"] = sum(
+            (item.quantity for item in basket_items), 0
+        )
         context["title"] = "Store - Оформление заказа"
-        context["form"] = OrderForm()
         return context
+
+    def form_valid(self, form):
+        basket_qs = Basket.objects.filter(user=self.request.user).select_related(
+            "product"
+        )
+        if not basket_qs.exists():
+            form.add_error(None, "Корзина пуста. Добавьте товары и попробуйте снова.")
+            return self.form_invalid(form)
+
+        basket_history = {
+            str(item.id): {
+                "product_name": item.product.name,
+                "quantity": item.quantity,
+                "price": str(item.product.price),
+                "sum": str(item.sum),
+            }
+            for item in basket_qs
+        }
+        Order.objects.create(
+            first_name=form.cleaned_data["first_name"],
+            last_name=form.cleaned_data["last_name"],
+            email=form.cleaned_data["email"],
+            address=form.cleaned_data["address"],
+            basket_history=basket_history,
+            initiator=self.request.user,
+        )
+        basket_qs.delete()
+        return super().form_valid(form)
 
 
 class OrderSuccessView(TemplateView):
