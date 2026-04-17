4981c42f4d707c82b711ce57fafa110c4f3a428d
ukarpenkov <truloveu@gmail.com>
2026-04-17 18:14:52 +0300

    add status display method and update list_display in OrderAdmin

--- store-server/orders/admin.py
+++ store-server/orders/admin.py
@@ -5,7 +5,21 @@ from orders.models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "email", "status", "created", "initiator")
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "status_display",
+        "status",
+        "created",
+        "initiator",
+    )
+    list_editable = ("status",)
     list_filter = ("status", "created")
     search_fields = ("id", "first_name", "last_name", "email", "initiator__username")
     readonly_fields = ("created",)
+
+    @admin.display(description="Статус")
+    def status_display(self, obj):
+        return obj.get_status_display()
