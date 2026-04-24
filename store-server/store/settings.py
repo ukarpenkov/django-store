f7f964724ebe09feee15f0b78116397fcbd52406
ukarpenkov <truloveu@gmail.com>
2026-04-24 22:20:03 +0300

    update STATICFILES_DIRS and STATIC_ROOT settings based on DEBUG value

--- store-server/store/settings.py
+++ store-server/store/settings.py
@@ -170,9 +170,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
-if not DEBUG:
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
     STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
