b70b6e0fba64147bc0e9c8ddd21f4b05422ee477
ukarpenkov <truloveu@gmail.com>
2026-04-24 10:47:58 +0300

    add django-extensions to requirements and INSTALLED_APPS

--- store-server/store/settings.py
+++ store-server/store/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "debug_toolbar",
     "allauth.account",
     "allauth.socialaccount.providers.github",
