9cf9038f4229913bd4d560da24713d254d80b277
ukarpenkov <truloveu@gmail.com>
2026-04-24 22:16:59 +0300

    add STATIC_ROOT setting for production environment

--- store-server/store/settings.py
+++ store-server/store/settings.py
@@ -172,6 +172,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
